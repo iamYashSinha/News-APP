daf52e14983a702c5f1918b16bb6185621bf0a58
sinhayash9546 <sinhayash9546@gmail.com>
2023-06-16 22:57:45 +0530

    added feature of fetching news cat wise

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Pages/Home";
-import Catogories from "./components/Pages/Catogories";
+import FetchData from "./components/FetchData";
 import Footer from "./components/Footer";
 function App() {
   return (
@@ -12,13 +12,13 @@ function App() {
         <Routes>
 
           <Route path="/" element={<Home />} />
-          <Route path="/general" element={<Catogories cat="general" />} />
-          <Route path="/business" element={<Catogories cat="business" />} />
-          <Route path="/entertainment" element={<Catogories cat="entertainment" />} />
-          <Route path="/health" element={<Catogories cat="health" />} />
-          <Route path="/science" element={<Catogories cat="science" />} />
-          <Route path="/sports" element={<Catogories cat="sports" />} />
-          <Route path="/technology" element={<Catogories cat="technology" />} />
+          <Route path="/general" element={<FetchData cat="general" />} />
+          <Route path="/business" element={<FetchData cat="business" />} />
+          <Route path="/entertainment" element={<FetchData cat="entertainment" />} />
+          <Route path="/health" element={<FetchData cat="health" />} />
+          <Route path="/science" element={<FetchData cat="science" />} />
+          <Route path="/sports" element={<FetchData cat="sports" />} />
+          <Route path="/technology" element={<FetchData cat="technology" />} />
 
         </Routes>
         <Footer />
--- src/components/Pages/Catogories.jsx
+++ src/components/Pages/Catogories.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Catogories(props) {
   return (
-    <div>
+    <div style={{minHeight: "100vh"}}>
       {props.cat}
     </div>
   )
